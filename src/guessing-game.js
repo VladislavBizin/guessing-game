85c2482eef78c15de22ae7ad9b20255a14a01f6d
VladislavBizin <vladislav.bizin@gmail.com>
2019-09-26 16:24:13 +0300

    feat: implement  class "GuessingGame"

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.floor = null;
+        this.ceil = null;
+    }
 
     setRange(min, max) {
-
+        this.floor = min;
+        this.ceil = max;
     }
 
     guess() {
+        return Math.ceil((this.floor + this.ceil) / 2);
 
     }
 
     lower() {
-
+        this.ceil = Math.ceil((this.floor + this.ceil) / 2);
     }
 
     greater() {
-
+        this.floor = Math.ceil((this.ceil + this.floor) / 2);
     }
 }
 
+
+
 module.exports = GuessingGame;
+
